e6879ec8d5ca250dfd2a94b6963db50e5efea868
Iraqsoft95 <iraqsoft1995@gmail.com>
2024-09-30 13:14:27 +0300

    Update index.cmd

--- index.cmd
+++ index.cmd
@@ -19,107 +19,107 @@ set PC_Name=%COMPUTERNAME%
 set Server_Name= .\SALES_DEV
 set SQL_Connecction= -S .\SALES_DEV  -U sa -P 12345
 set SQLCMD="C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\170\Tools\Binn\sqlcmd.exe"
-set Download_Path=
+set "Download_Path=%USERPROFILE%\Downloads"
 @REM -------------------------> Downloads Info
 @REM ----------> SPEEDOO POS 
 set SPEEDOO_POS_FULL_URL="https://www.dropbox.com/scl/fi/inwa5nl2ua5hlm1blb7rf/SPEEDOO-POS-1.3.8.4-FULL.exe?rlkey=kpqtgcwg490kxqo01h0ygz746&e=1&dl=0"
-set SPEEDOO_POS_FULL_FILE="SPEEDOO-POS-1.3.8.4-FULL.exe"
+set SPEEDOO_POS_FULL_FILE="%Download_Path%\SPEEDOO-POS-1.3.8.4-FULL.exe"
 set SPEEDOO_POS_UPDATE_URL= "https://www.dropbox.com/scl/fi/p5svl5yfmihdyuva6c158/SPEEDOO-POS-1.3.8.4-UPDATE.exe?rlkey=tms77f0sc9xe5he1l2m9mmvxu&e=1&dl=0"
-set SPEEDOO_POS_UPDATE_FILE= "SPEEDOO-POS-1.3.8.4-UPDATE.exe"
+set SPEEDOO_POS_UPDATE_FILE= "%Download_Path%\SPEEDOO-POS-1.3.8.4-UPDATE.exe"
 set SPEEDOO_POS_CLINT_URL= "https://www.dropbox.com/scl/fi/cvh53cxfptgrbl4th9ybf/SPEEDOO-POS-1.3.8.4-CLIENTS.exe?rlkey=engqaag83qnl4584g8hey1e3f&e=1&dl=0"
-set SPEEDOO_POS_CLINT_FILE= "SPEEDOO-POS-1.3.8.4-CLIENTS.exe"
+set SPEEDOO_POS_CLINT_FILE= "%Download_Path%\SPEEDOO-POS-1.3.8.4-CLIENTS.exe"
 
 @REM -----------> SPEEDOO REST 
 set SPEEDOO_REST_FULL_URL= "https://www.dropbox.com/scl/fi/dnyporwaojb1iysma0lk4/Speedoo-APP-3.0.5.7-FULL.exe?rlkey=b2rgp9yxvz2hn2cil5zv37she&st=1sulgbl7&dl=0"
-set SPEEDOO_REST_FULL_FILE= "Speedoo-APP-3.0.5.7-FULL.exe"
+set SPEEDOO_REST_FULL_FILE= "%Download_Path%\Speedoo-APP-3.0.5.7-FULL.exe"
 set SPEEDOO_REST_UPDATE_URL= "https://www.dropbox.com/scl/fi/fsv5smyvtychtmqn3ncia/Speedoo-REST-3.0.5.7-CLIENTS.exe?rlkey=bg8zzdlkzl7g2h89367kljzg7&e=1&st=xiesetls&dl=0"
-set SPEEDOO_REST_UPDATE_FILE= "Speedoo-REST-3.0.5.7-CLIENTS.exe"
+set SPEEDOO_REST_UPDATE_FILE= "%Download_Path%\Speedoo-REST-3.0.5.7-CLIENTS.exe"
 set SPEEDOO_REST_CLINT_URL= "https://www.dropbox.com/scl/fi/39s6a36r0hzj7g8bgnbyd/Speedoo-REST-3.0.5.7-UPDATE.exe?rlkey=hv665vott91ebgngbxeu8mvt6&e=1&st=x78dtckx&dl=0"
-set SPEEDOO_REST_CLINT_FILE= "Speedoo-REST-3.0.5.7-UPDATE.exe"
+set SPEEDOO_REST_CLINT_FILE= "%Download_Path%\Speedoo-REST-3.0.5.7-UPDATE.exe"
 
 @REM -----------> Point_Pos
 set Point_Pos_FULL_URL= "https://www.dropbox.com/s/ne3uk78dl3po1ak/IraqSoft_point%20_pos%203.9.2.exe?dl=0"
-set Point_Pos_FULL_FILE= "point_pos_03.9.2.exe"
+set Point_Pos_FULL_FILE= "%Download_Path%\point_pos_03.9.2.exe"
 set Point_Pos_UPDATE_URL= "https://www.dropbox.com/scl/fi/iu59y1onbs5a3mxytpvzb/Point_3.9.2_update.exe?rlkey=mbzmbcltitcb892b9q7tfkqxn&st=07kdwpa3&dl=0"
-set Point_Pos_UPDATE_FILE= "Point_3.9.2_update.exe"
+set Point_Pos_UPDATE_FILE= "%Download_Path%\Point_3.9.2_update.exe"
 
 @REM -----------> Point_Super 
 set Point_Super_FULL_URL= "https://www.dropbox.com/scl/fi/c626z7d3ijhu0uqal0vii/Point_Super_Full_3.0.0.2.exe?rlkey=oielyjj51n43puuf140bafsqt&st=xcpqcyfv&dl=0"
-set Point_Super_FULL_FILE= "Point_Super_Full_3.0.0.2.exe"
+set Point_Super_FULL_FILE= "%Download_Path%\Point_Super_Full_3.0.0.2.exe"
 set Point_Super_UPDATE_URL= "https://www.dropbox.com/scl/fi/60oxrlyzqh60wkzlmr61g/Point_Super_Update_3.0.0.2.exe?rlkey=jcqs1mlq0kjhe64zkulzmnxyf&st=dd6mdkma&dl=0"
-set Point_Super_UPDATE_FILE= "Point_Super_Update_3.0.0.2.exe"
+set Point_Super_UPDATE_FILE= "%Download_Path%\Point_Super_Update_3.0.0.2.exe"
 set Point_Super_CLINT_URL= "https://www.dropbox.com/scl/fi/53ukv2pj0aw0ozxwlsitz/Point_Super_Client_3.0.0.2.exe?rlkey=fk5ywhepeisnnfr3ysgovnu07&st=ppwkir9r&dl=0"
-set Point_Super_CLINT_FILE= "Point_Super_Client_3.0.0.2.exe"
+set Point_Super_CLINT_FILE= "%Download_Path%\Point_Super_Client_3.0.0.2.exe"
 
 @REM -----------> Shipping_Plus_2.2.0.0
 set Shipping_Plus_2.2.0.0_FULL_URL= "https://www.dropbox.com/scl/fi/ok2td062y7qgxisofyyxl/Shipping-Plus-Server-2.2.0.0.exe?rlkey=8gw2o04va9m3rrfzpglrx47ev&st=tqqjzjj6&dl=0"
-set Shipping_Plus_2.2.0.0_FULL_FILE= "Shipping-Plus-Server-2.2.0.0.exe"
+set Shipping_Plus_2.2.0.0_FULL_FILE= "%Download_Path%\Shipping-Plus-Server-2.2.0.0.exe"
 set Shipping_Plus_2.2.0.0_UPDATE_URL= "https://www.dropbox.com/scl/fi/sfv9t2hf1wpge9lsun3n1/Shipping_Plus_UBDAT_2.2.0.0.exe?rlkey=arpv9nzs6ps9nwv0gh38ds4ck&st=cjjgffqg&dl=0"
-set Shipping_Plus_2.2.0.0_UPDATE_FILE= "Shipping_Plus_UBDAT_2.2.0.0.exe"
+set Shipping_Plus_2.2.0.0_UPDATE_FILE= "%Download_Path%\Shipping_Plus_UBDAT_2.2.0.0.exe"
 set Shipping_Plus_2.2.0.0_CLINT_URL= "https://www.dropbox.com/scl/fi/i6yqud8l6k9p8eck4k3d0/Shipping-Plus-Client-2.2.0.0.exe?rlkey=1fs4ydwr4repco3aadtyef40c&st=5q6lomox&dl=0"
-set Shipping_Plus_2.2.0.0_CLINT_FILE= "Shipping-Plus-Client-2.2.0.0.exe"
+set Shipping_Plus_2.2.0.0_CLINT_FILE= "%Download_Path%\Shipping-Plus-Client-2.2.0.0.exe"
 
 @REM -----------> Shipping_Plus_2.2.2.2
 set Shipping_Plus_2.2.2.2_FULL_URL= "https://www.dropbox.com/scl/fi/s1g97nsh7b2fcm58uxqdu/Shipping-Plus-Server2.2.2.2.exe?rlkey=hsktgir1wmrvbb4nrq69qw80l&st=9ite619r&dl=0"
-set Shipping_Plus_2.2.2.2_FULL_FILE= "Shipping-Plus-Server-2.2.2.2.exe"
+set Shipping_Plus_2.2.2.2_FULL_FILE= "%Download_Path%\Shipping-Plus-Server-2.2.2.2.exe"
 set Shipping_Plus_2.2.2.2_UPDATE_URL= "https://www.dropbox.com/scl/fi/7ei98af4bxjiwbwprmici/Shipping_Plus_Server_UBDAT2.2.2.2.exe?rlkey=r4d6xxylgfdwdywjekm4w879w&st=eauqw0ni&dl=0"
-set Shipping_Plus_2.2.2.2_UPDATE_FILE= "Shipping_Plus_UBDAT_2.2.2.2.exe"
+set Shipping_Plus_2.2.2.2_UPDATE_FILE= "%Download_Path%\Shipping_Plus_UBDAT_2.2.2.2.exe"
 
 @REM -----------> SQL 
 set Sql_2008_URL= "https://edef7.pcloud.com/cfZizHfU9Zrqiq0cZya0SZZjfkPkkZ2ZZRkHZZd0NSFZa5ZMkZj7ZsHZkpZUJZzpZDkZDVZDXZ20Z7pZ8pZfXZHWedlp26mcm5JWCnEhIk0zzpRb7y/SQL2008.rar"
-set Sql_2008_FILE="SQL2008.rar"
+set Sql_2008_FILE="%Download_Path%\SQL2008.rar"
 set Sql_2012_URL= "https://edef4.pcloud.com/cfZbtOfU9ZW7060cZya0SZZ9YVPkkZ2ZZRkHZZEFVamZb5Zs5Zr7ZpFZp5ZeVZu7ZXFZI7ZEXZ05ZrXZUJZVHZCViumTowyFVEbra2tdAtjBWs6mXX/SQL2012.rar"
-set Sql_2012_FILE="SQL2012.rar"
+set Sql_2012_FILE="%Download_Path%\SQL2012.rar"
 set Sql_2014_URL= "https://edef6.pcloud.com/cfZoDofU9ZGys60cZya0SZZzmVPkkZ2ZZRkHZZfJR7RZd0ZPVZdJZvJZJ0ZPFZoJZJkZ6FZOHZiFZcHZNFZWkZbYeqV08OfBBiz2wr9hlPB8TJYDGk/SQL2014.rar"
-set Sql_2014_FILE="SQL2014.rar"
+set Sql_2014_FILE="%Download_Path%\SQL2014.rar"
 set Sql_2019_URL= "https://edef11.pcloud.com/DLZ2vOfU9Zvb060cZya0SZZEmVPkkZ2ZZRkHZZiO5XZaJZAHZbkZfax53T9wWbVpmgHL5uDtJX3Xkt57/SQL2019.rar"
-set Sql_2019_FILE1="SQL2019.rar"
+set Sql_2019_FILE1="%Download_Path%\SQL2019.rar"
 
 @REM -----------> Aman 
 set Aman_URL="https://www.dropbox.com/scl/fi/mmb09081w93d4o11b6q6o/AMAN_AutoBackup.exe?rlkey=yi81hsbjsmml5zwjjmfvr78cc&st=kcd1p6d8&dl=0"
-set Aman_FILE="AMAN_AutoBackup.exe"
+set Aman_FILE="%Download_Path%\AMAN_AutoBackup.exe"
 
 @REM -----------> Service_Programs 
 set dControl_URL="https://edef2.pcloud.com/D4Zs71KE9ZatVgkcZya0SZZoSdgkkZ2ZZRkHZkZYF7kZ9kZSkZmXZT6hPZJ7K5xXlicwuUfYdN1dcHMmvzOjfX/dControl.zip"
-set dControl_FILE="dControl.zip"
+set dControl_FILE="%Download_Path%\dControl.zip"
 set WUB_URL="https://edef6.pcloud.com/D4ZhHm4fuZE9ul7cZya0SZZEUlgkkZ2ZZRkHZkZI4dVZ57ZSVZ40ZjLhPZhftQuNgsDx5X6RjSbVF8shAUVgu7/Wub_v1.8.zip"
-set WUB_FILE="Wub_v1.8.zip"
+set WUB_FILE="%Download_Path%\Wub_v1.8.zip"
 set Windows_Activity_URL="https://edef12.pcloud.com/cfZE5eKE9ZyA5gkcZya0SZZVedgkkZ2ZZRkHZZe7eQZj0ZTXZmHZ1HZLpZnkZRHZ10ZQpZLHZjXZEVZiFZF5Z6On8aupWMkuoN0inS6PNvf4aEhRy/KMSAuto%20Net.rar"
-set Windows_Activity_FILE= "KMSAutoNet.rar"
+set Windows_Activity_FILE= "%Download_Path%\KMSAutoNet.rar"
 set Winrar_URL="https://www.rarlab.com/rar/winrar-x64-701ar.exe"
-set Winrar_FILE="winrar-x64-701ar.exe"
+set Winrar_FILE="%Download_Path%\winrar-x64-701ar.exe"
 
 @REM -------------------------> Printers Info
 @REM -----------> Canon_6030 Url
 set Canon_6030_Url="https://www.dropbox.com/scl/fi/m2tznhch5h3q3t9c1aeti/LBP6030_V2111_WP_EN.exe?rlkey=3moh1m0vvk05bres72ygpglrc&st=s0km86rh&dl=0"
-set Canon_6030_File="LBP6030_V2111_WP_EN.exe"
+set Canon_6030_File="%Download_Path%\LBP6030_V2111_WP_EN.exe"
 @REM -----------> Thermal printer
 set pos_Url="https://www.dropbox.com/scl/fi/h3g3y0s19ael2fhl2z2kg/Thermal_printer.exe?rlkey=xcueh3f0jcm8ub9ia3sakp431&st=788nqj3v&dl=0"
-set pos_File="Thermal_printer.exe"
+set pos_File="%Download_Path%\Thermal_printer.exe"
 @REM -----------> BARCODE printer
 set BARCODE_Url="https://www.dropbox.com/scl/fi/jnpe5h17kqu0w1ahf7cqa/4BARCODE_2019.1_M-2.exe?rlkey=n821h1heahvysemcl98k7imod&st=ht6hl92f&dl=0"
-set BARCODE_File="4BARCODE_2019.1_M-2.exe"
+set BARCODE_File="%Download_Path%\4BARCODE_2019.1_M-2.exe"
 @REM -----------> IPOS JJ
 set IPOS_JJ_Url="https://www.dropbox.com/scl/fi/oflbr8m52bjr7ilrw5mfh/JJ-Drive-V7.17.exe?rlkey=tgge9ayjq25hp05cihg7zk5lv&st=5bczxlv3&dl=0"
-set IPOS_JJ_File="JJ-Drive-V7.17.exe"
+set IPOS_JJ_File="%Download_Path%\JJ-Drive-V7.17.exe"
 @REM -----------> POS 80 Series
 set POS_80_Series_Url="https://www.dropbox.com/scl/fi/lh17yiq8squudlgw9d86i/POS-80-Series.exe?rlkey=60ii8te041ssix5nnjw3noexu&st=n69hmf7t&dl=0"
-set POS_80_Series_File="POS-80-Series.exe"
+set POS_80_Series_File="%Download_Path%\POS-80-Series.exe"
 @REM -----------> AG POS Printer
 set AG_POS_Url="https://www.dropbox.com/scl/fi/iyai1v98115j30rgg0dux/AG_Printer.exe?rlkey=ebpsa8h8twq9isj96lrz6xcpi&st=q1q5reis&dl=0"
-set AG_POS_File="AG_Printer.exe"
+set AG_POS_File="%Download_Path%\AG_Printer.exe"
 @REM -----------> ZJ_Printer
 set ZJ_Url="https://www.dropbox.com/scl/fi/iimc78o9ow8uupi9hoqtf/ZJ-Printer.exe?rlkey=cgbbzhbebh4ha4hj7sfsq0mrx&st=k0e5khaw&dl=0"
-set ZJ_File="ZJ-Printer.exe"
+set ZJ_File="%Download_Path%\ZJ-Printer.exe"
 @REM -----------> XPrinter
 set xpriner_Url="https://www.dropbox.com/scl/fi/cqt1vbk36ne7byxwih95l/XPrinter.exe?rlkey=4dkwwg36y948qo6ufjib1e4oj&st=5p1naka3&dl=0"
-set xpriner_File="XPrinter.exe"
+set xpriner_File="%Download_Path%\XPrinter.exe"
 @REM -----------> HPRT Printer
 set HPRT_Url="https://www.dropbox.com/scl/fi/mcc04owwq75jfaa9dj4na/HPRT_2022.1.exe?rlkey=yqfs4w0hp4hwhixmlas4mebbf&st=aylovokd&dl=0"
-set HPRT_File="HPRT_2022.1.exe"
+set HPRT_File="%Download_Path%\HPRT_2022.1.exe"
 @REM -----------> Printer Tool
 set Printer_Tool_Url="https://www.dropbox.com/scl/fi/w36cj9klh00ginyhp21wq/printer-tools.rar?rlkey=oj1imyo9hvenqf7txcyp6nqcj&st=ejiwbu1o&dl=0"
-set Printer_Tool_File="printer-tools.rar"
+set Printer_Tool_File="%Download_Path%\printer-tools.rar"
 
 @REM -------------------------> Main_Menu <----------------------------- 
 :Main_Menu
